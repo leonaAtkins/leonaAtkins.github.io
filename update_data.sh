ffb39426a6325242ae6145e3d4f0dab245afbace
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-08-13 16:05:09 +0100

    update

---
+++ update_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl https://www.metoffice.gov.uk/public/data/LayerCache/GetCapabilities/Item/Forecast > ./assets/data/Forecast.xml && curl https://www.metoffice.gov.uk/public/data/LayerCache/GetCapabilities/Item/Observation > ./assets/data/observation.xml
