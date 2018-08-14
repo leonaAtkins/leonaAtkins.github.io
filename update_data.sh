0fb279ec1a054cd7c4c8a1ee2cd3368fa8844b12
Atkins, Leona <leona.atkins@metoffice.gov.uk>
2018-08-14 09:47:24 +0100

    update '  '

--- update_data.sh
+++ update_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TIMESTAMP = $(date -I)
+TIMESTAMP = $(date -u +%FT%TZ)
 curl https://www.metoffice.gov.uk/public/data/LayerCache/GetCapabilities/Item/Forecast > ./assets/data/Forecast.xml && curl https://www.metoffice.gov.uk/public/data/LayerCache/GetCapabilities/Item/Observation > ./assets/data/observation.xml
 git add --all
-git commit -m "update $TIMESTAMP"
+git commit -m "update ' $TIMESTAMP ' "
 git push -u origin master
